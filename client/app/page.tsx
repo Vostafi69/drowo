dea01e937a22bd0750fdbe28ee064e2bed8b2a1e
Alexander <a.thorik@yandex.ru>
2024-07-15 00:16:31 +1000

    add: добавлен файл docker-compose и .prettierignore

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,20 @@
-export default function Home() {
-  const someV = "dqwdw";
+"use client";
 
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>;
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+export default function Home() {
+  const [data, setData] = useState<string>("Запросить данные");
+  const [isLoading, setIsloading] = useState<boolean>(false);
+
+  const fetchData = () => {
+    setIsloading(true);
+    fetch("http://localhost:5000/users/")
+      .then((data) => data.json())
+      .then((data) => setData(data.message))
+      .catch((err) => console.log(err))
+      .finally(() => setIsloading(false));
+  };
+
+  return <Button onClick={fetchData}>{isLoading ? "Загрузка" : data}</Button>;
 }
