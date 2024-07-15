feb4108163c983518cb7c908c740c3300b6d0e07
Alexander <a.thorik@yandex.ru>
2024-07-15 22:02:47 +1000

    add: добавлен новый основнйо роут dashboard

---
+++ client/app/(dashboard)/layout.tsx
@@ -0,0 +1,26 @@
+import { Navbar } from "@/src/features";
+import { OrgSidebar, Sidebar } from "@/src/widgets";
+import { ReactNode } from "react";
+
+interface DashboardLayoutProps {
+  children: ReactNode;
+}
+
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  return (
+    <main className="h-full">
+      <Sidebar />
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            <Navbar />
+            {children}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default DashboardLayout;
---
+++ client/app/(dashboard)/page.tsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+  return <>DashBoardRootPage</>;
+};
+
+export default Dashboard;
